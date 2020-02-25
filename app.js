a9f5d2106de4f3382266013a048add3a72fdd672
Lu-Ma-Ne <luguneves@gmail.com>
2020-02-25 20:44:07 +0000

    commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+  var planets = ['Jupiter', 'Saturn', 'Venus', 'Earth', 'Mars'];
+  console.log(planets[0], planets[1], planets[2], planets[3], planets[4]);
+
+var wowDatatypes = [int=1, char='L', double=20.15];
+console.log(wowDatatypes);
+
+var x=5;
+var y=10;
+var z=15;
+console.log(x+y);
+console.log(y-x);
+console.log(x*y);
+console.log(y/x);
+console.log(x%2);
+console.log(x++);
+console.log(x--);
+
+var a=5;
+var b="Hello";
+var c="World!";
+var d=10;
+console.log(a+b);
+console.log(b+c);
+console.log(a+d);
+
+var a=5;
+var b=10;
+var c="5";
+var x=a;
+console.log(a==c);
+console.log(a===c);
+console.log(a==x);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a>=c);
+console.log(a<=c);
+
+var hello = function() {
+    return 'Hi!';
+};
+var x = hello();
+console.log (x);
+
+
